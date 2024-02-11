5410f4686a7258417eccb3e3eac9899d673eb25c
Егор Егор <you@example.com>
2024-02-11 19:53:17 +0300

    Refactor structure, move db models from database.py to models.py

---
+++ app/DAO.py
@@ -0,0 +1,31 @@
+from database import engine, Session, ProductsMapping
+
+# Создание таблицы ProductsMapping, если она еще не создана
+ProductsMapping.__table__.create(bind=engine, checkfirst=True)
+
+# Функция для добавления тестовой записи в таблицу ProductsMapping
+def add_test_product_mapping():
+    # Создаем новый объект сессии
+    session = Session()
+
+    # Проверяем, существует ли уже запись с таким marketplace_id и external_code
+    exists = session.query(ProductsMapping.id).filter_by(marketplace_id=1, external_code=17631627).scalar() is not None
+    if not exists:
+        # Создаем тестовую запись
+        test_mapping = ProductsMapping(
+            marketplace_id=1,  # ID для маркетплейса Wildberries
+            internal_code=12345678,  # Пример внутреннего артикула
+            external_code=17631627  # Пример артикула с маркетплейса Wildberries
+        )
+        # Добавляем запись в сессию и коммитим транзакцию
+        session.add(test_mapping)
+        session.commit()
+        print(f"Добавлена тестовая запись с internal_code {test_mapping.internal_code} и external_code {test_mapping.external_code}")
+    else:
+        print("Тестовая запись уже существует.")
+
+    # Закрываем сессию
+    session.close()
+
+# Вызов функции для добавления тестовой записи
+add_test_product_mapping()
---
+++ app/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class ProductsMapping(Base):
+    __tablename__ = 'products_mapping'
+
+    id = Column(Integer, primary_key=True)
+    marketplace_id = Column(Integer)
+    internal_code = Column(Integer)
+    external_code = Column(Integer, unique=True)
+
+
+class Feedback(Base):
+    __tablename__ = 'feedbacks'
+
+    id = Column(String, primary_key=True)
+    text = Column(String)
+    product_valuation = Column(Integer)
+    created_date = Column(DateTime)
+    answer = Column(String)
+    state = Column(String)
+    imt_id = Column(Integer)
+    nm_id = Column(Integer, ForeignKey('products_mapping.external_code'))
+    product_name = Column(String)
+    supplier_article = Column(String)
+    supplier_name = Column(String)
+    brand_name = Column(String)
+    size = Column(String)
+    video = Column(String)
+    was_viewed = Column(Boolean)
+    photo_links = Column(String)
+    user_name = Column(String)
+    matching_size = Column(String)
+    is_able_supplier_feedback_valuation = Column(Boolean)
+    supplier_feedback_valuation = Column(Integer)
+    is_able_supplier_product_valuation = Column(Boolean)
+    supplier_product_valuation = Column(Integer)
+    is_able_return_product_orders = Column(Boolean)
+    return_product_orders_date = Column(DateTime)
+    bables = Column(String)
+
+    # Связь с таблицей соответствия артикулов
+    product_mapping = relationship('ProductsMapping', backref='feedbacks')
