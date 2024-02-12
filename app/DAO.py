a1dc7a162151829d3517687ab86ab0aebe94097f
Егор Егор <you@example.com>
2024-02-12 22:34:10 +0300

    temporary commit

--- app/DAO.py
+++ app/DAO.py
@@ -1,42 +1,36 @@
-from sqlalchemy.orm import Session
-from app.models import ProductsMapping, Feedback
+from contextlib import contextmanager
+from app.database import engine, Session
+from app.models import Feedback, ProductsMapping
 
-class ReviewDataAccess:
-    def __init__(self, session: Session):
-        self.session = session
+@contextmanager
+def session_scope():
+    """Предоставляет область действия для сессии."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
-    def add_review(self, review_data: dict):
-        # Предполагаем, что данные уже прошли валидацию и готовы к вставке
-        review = Feedback(**review_data)
-        self.session.add(review)
-        self.session.commit()
 
-    def add_bulk_reviews(self, reviews_data: list):
-        # Предполагаем, что данные уже прошли валидацию и готовы к вставке
-        self.session.bulk_insert_mappings(Feedback, reviews_data)
-        self.session.commit()
+class ReviewDAO:
 
-    def get_last_review_date(self, external_code: int):
-        # Получаем дату последнего отзыва по артикулу
-        last_review = (self.session.query(Feedback.created_date)
-                       .join(ProductsMapping, ProductsMapping.external_code == Feedback.nm_id)
-                       .filter(ProductsMapping.external_code == external_code)
-                       .order_by(Feedback.created_date.desc())
-                       .first())
-        return last_review[0] if last_review else None
+    def add_reviews_to_database(self, reviews):
+        with session_scope() as session:
+            for review_data in reviews:
+                review = Feedback(**review_data)
+                session.add(review)
 
-# Пример использования DAO
-# db_session - это объект сессии SQLAlchemy, который вы создаете, когда запускаете ваше приложение
-review_dao = ReviewDataAccess(db_session)
-# Добавление нового отзыва
-review_dao.add_review({
-    'text': 'Отличный товар',
-    # ...остальные данные отзыва
-})
-# Добавление группы отзывов
-review_dao.add_bulk_reviews([
-    {'text': 'Отличный товар', 'nm_id': 123456, 'created_date': '2022-01-01'},
-    # ...остальные отзывы
-])
-# Получение даты последнего отзыва по артикулу
-last_date = review_dao.get_last_review_date(external_code=123456)
+    @staticmethod
+    def get_max_review_date_by_article(external_code):
+        with session_scope() as session:
+            max_date = session.query(Feedback.created_date)\
+                .join(ProductsMapping, ProductsMapping.external_code == Feedback.nm_id)\
+                .filter(ProductsMapping.external_code == external_code)\
+                .order_by(Feedback.created_date.desc())\
+                .first()
+            return max_date[0] if max_date else None
+
--- app/loader.py
+++ app/loader.py
@@ -1,56 +1,104 @@
-from app.database import engine, Session
-from app.models import Feedback, ProductsMapping
-from contextlib import contextmanager
+import time
+from datetime import datetime
+
+from app.DAO import ReviewDAO
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv(override=True)
+
+API_TOKEN = os.getenv("api_token")
+max_per_request = 5000
 
 
+class ReviewsLoader:
 
-class Loader:
-    def __init__(self):
-        self.session_factory = Session
-
-    @contextmanager
-    def session_scope(self):
-        """Предоставляет транзакционную область действия для сессии."""
-        session = self.session_factory()
-        try:
-            yield session
-            session.commit()
-        except Exception:
-            session.rollback()
-            raise
-        finally:
-            session.close()
-
-    def get_max_review_date_by_article(self, external_code):
-        with self.session_factory() as session:
-            try:
-                max_date = session.query(Feedback.created_date). \
-                    join(ProductsMapping, ProductsMapping.external_code == Feedback.nm_id). \
-                    filter(ProductsMapping.external_code == external_code). \
-                    order_by(Feedback.created_date.desc()). \
-                    first()
-
-                if max_date:
-                    return max_date[0]
-                else:
-                    return None
-
-    def load_reviews(self, external_code):
-        max_review_date = self.get_max_review_date_by_article(external_code)
+    def get_reviews(self, external_code, is_answered):
+        max_review_date = ReviewDAO.get_max_review_date_by_article(external_code)
 
         if max_review_date is not None:
-            # Преобразуем дату в Unix timestamp
             date_from = int(max_review_date.timestamp())
-            # Теперь устанавливаем параметры запроса к API с date_from
-            api_params = {
-                'date_from': date_from,
-                # Остальные параметры...
-            }
         else:
-            # Параметры запроса к API без date_from, чтобы получить все отзывы
-            api_params = {
-                # Остальные параметры...
-            }
+            date_from = None
 
-        # Остальная логика загрузки отзывов...
+        base_url = 'https://feedbacks-api.wildberries.ru/api/v1/feedbacks'
+        headers = {'Authorization': f'{API_TOKEN}'}
+        params = {
+            'dateFrom': date_from,
+            'nmId': external_code,
+            'take': max_per_request,
+            'skip': 0,
+            'isAnswered': is_answered,
+            'order': 'dateAsc'
+        }
 
+        reviews = []
+
+        while True:
+            response = requests.get(base_url, headers=headers, params=params)
+            if response.status_code != 200:
+                print(f"Ошибка запроса: {response.status_code}")
+                break
+
+            data = response.json()['data']['feedbacks']
+            reviews.extend(data)
+
+            if len(data) < max_per_request:
+                break
+
+            params['skip'] += max_per_request
+            time.sleep(1)
+        return reviews
+
+    def aggregate_reviews(self, external_code):
+        answered_reviews = self.get_reviews(external_code, True)
+        unanswered_reviews = self.get_reviews(external_code, False)
+        all_reviews = answered_reviews + unanswered_reviews
+        all_reviews.sort(key=lambda review: review['createdDate'])
+        # Преобразование каждого отзыва
+        transformed_reviews = [self.transform_review_data(review) for review in all_reviews]
+        return transformed_reviews
+
+    def transform_review_data(self, review_data):
+        # Функция для преобразования строки в формат datetime
+        def parse_datetime(date_str):
+            return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ') if date_str else None
+        # Преобразование ключей из API в ключи модели
+        transformed = {
+            'id': review_data['id'],
+            'text': review_data['text'],
+            'product_valuation': review_data['productValuation'],
+            'created_date': parse_datetime(review_data['createdDate']),
+            "answer": review_data["answer"],
+            "state": review_data["state"],
+            # Для поля "video", если оно может быть None
+            "video": review_data["video"] if review_data["video"] is not None else '',
+            "was_viewed": review_data["wasViewed"],
+            # Для поля "photoLinks", если оно может быть None
+            "photo_links": review_data["photoLinks"] if review_data["photoLinks"] is not None else '',
+            "user_name": review_data["userName"],
+            "matching_size": review_data["matchingSize"],
+            "is_able_supplier_feedback_valuation": review_data["isAbleSupplierFeedbackValuation"],
+            "supplier_feedback_valuation": review_data["supplierFeedbackValuation"],
+            "is_able_supplier_product_valuation": review_data["isAbleSupplierProductValuation"],
+            "supplier_product_valuation": review_data["supplierProductValuation"],
+            "is_able_return_product_orders": review_data["isAbleReturnProductOrders"],
+            "return_product_orders_date": review_data["returnProductOrdersDate"],
+            # Также может потребовать преобразования
+            "bables": review_data["bables"] if review_data["bables"] is not None else ''
+        }
+
+        # Вложенный словарь productDetails
+        product_details = review_data['productDetails']
+        transformed.update({
+            'imt_id': product_details['imtId'],
+            'nm_id': product_details['nmId'],
+            'product_name': product_details['productName'],
+            "supplier_article": product_details["supplierArticle"],
+            "supplier_name": product_details["supplierName"],
+            "brand_name": product_details["brandName"],
+            "size": product_details["size"]
+        })
+
+        return transformed
